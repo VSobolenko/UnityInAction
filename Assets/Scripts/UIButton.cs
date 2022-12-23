8250ee863aaa1328adc4f33156177fedc3085c68
VladislavS <v.sobolenko.vs@gmail.com>
2022-12-23 13:00:53 +0300

    2D Game Memory

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMassage;
+    public Color highlightColor = Color.cyan;
+
+    private void OnMouseEnter()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+
+    private void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMassage);
+        }
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Unity in Action (2nd ed)
-All of the sample projects from the book.
-
-Book website:
-https://www.manning.com/books/unity-in-action-second-edition?a_aid=newarteest&a_bid=512b6b05
